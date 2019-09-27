3e12a7267dd7b68d68a13986272c466570675e89
Art <afewell@gmail.com>
2019-09-27 05:16:36 -0700

    516

--- root/.callGitmir.cgi
+++ root/.callGitmir.cgi
@@ -1,4 +1,4 @@
 #!/usr/bin/perl
 print "Content-type: text/html\n\n";
 print "Hello, World.";
-exec "bash /gitmir/gitmir.sh -f /gitmir/feederFile.json";
+exec "gitmir -f /gitmir/feederFile.json";
--- root/.start.sh
+++ root/.start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 export GITMIRROOT
-chmod +x /gitmir/gitmir.sh
-/gitmir/gitmir.sh -f /gitmir/feederFile.json | tee /gitmir/gitmirlog
+gitmir -f /gitmir/feederFile.json | tee /gitmir/gitmirlog
 wait
 httpd-foreground
